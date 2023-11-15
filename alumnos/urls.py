973c93a2f794a42fa1d1ee538d1b272d01100744
Davidgf27 <148473628+Davidgf27@users.noreply.github.com>
2023-11-14 20:22:53 -0600

    Delete alumnos directory

--- alumnos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from alumnos import views
-
-urlpatterns = [
-    path('home',views.home),
-    path('saludo',views.saludo),
-]
--- alumnos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("<h1>Hola David, este es el home de tu proyecto</h1><a href='saludo'>Ir a saludo</a>")
-
-def saludo(request):
-    return HttpResponse("<h1>Hola David, bienvenido al mundo Django</h1><a href='home'>Ir a saludo</a>")
